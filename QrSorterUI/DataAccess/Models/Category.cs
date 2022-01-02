fc920e05f26f06c3a00f4e86e0787b3ca2197dc6
wiktoriakeller <wiktoriakeller552@gmail.com>
2022-01-02 10:19:07 +0100

    Add init files for UI

---
+++ QrSorterUI/DataAccess/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string CategoryName { get; set; }
+        public int NumberOfProducts { get; set; }
+        public int BoxId { get; set; }
+    }
+}
